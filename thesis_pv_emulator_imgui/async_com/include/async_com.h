3d9bd7924bae5d74737160a3dac4d38ec509bb6d
Chris Konstantinou <konstantinou@lms.mech.upatras.gr>
2023-11-25 16:27:33 +0200

    feat: added missing constructor (minor)

--- thesis_pv_emulator_imgui/async_com/include/async_com.h
+++ thesis_pv_emulator_imgui/async_com/include/async_com.h
@@ -3,5 +3,16 @@
 class AsyncCommunication
 {
 public:
+	AsyncCommunication();
+	~AsyncCommunication();
+	/*
+		A method to test Async write
+	*/
 	void Test(void);
+
+	/*
+		Get data from a COM port, TODO
+	*/
+	void GetDatafromCOMPort(void);
+
 };
--- thesis_pv_emulator_imgui/async_com/src/async_com.cpp
+++ thesis_pv_emulator_imgui/async_com/src/async_com.cpp
@@ -14,21 +14,37 @@ extern double rt_p[1];
 // Test if we have acess to PV class
 extern PV::PVModule pvModule; // main PV module handle
 
+AsyncCommunication::AsyncCommunication()
+{
+	std::cout << "AsyncCommunication Initialized" << std::endl;
+}
+
+AsyncCommunication::~AsyncCommunication()
+{
+	std::cout << "AsyncCommunication Destroyed" << std::endl;
+}
+
+
 void AsyncCommunication::Test()
 {
 	while (true)
 	{
-		for (int i = 0; i < 35 * 2; i++)
+		for (int i = 0; i < 35 * 4; i++)
 		{
 			// std::lock_guard<std::mutex> lock(mtx); // FIX MUTEX ISSUE LATER
 			
-			rt_v[0] = (double)i / 2.0;
+			rt_v[0] = (double)i / 4.0;
 			
 			rt_i[0] = pvModule.GetCurrentFromVoltage(rt_v[0]);
 
 			rt_p[0] = rt_v[0] * rt_i[0];
 
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
 		}
 	}
+}
+
+void AsyncCommunication::GetDatafromCOMPort()
+{
+
 }
